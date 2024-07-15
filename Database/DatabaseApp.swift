7101db61554079cdc5d4741956ebc8145d26b6bf
pieterjooste <pieter.jooste.jp@gmail.com>
2024-07-15 12:17:09 +0200

    Initial Commit

---
+++ Database/DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseApp.swift
+//  Database
+//
+//  Created by Pieter Jooste on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
